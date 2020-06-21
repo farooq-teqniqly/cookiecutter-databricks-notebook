50d407a64a5338ed8f3ab55a637a232ee43f852f
Farooq Mahmud <farooq@teqniqly.com>
2020-06-20 17:00:35 -0700

    First.

---
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `{{ cookiecutter.project_slug }}` package."""
+
+
+import unittest
+
+from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+
+
+class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+    """Tests for `{{ cookiecutter.project_slug }}` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
