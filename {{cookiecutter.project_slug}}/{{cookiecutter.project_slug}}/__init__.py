03db8f3793f6939f08c479f432fc1f25a99c8262
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-06-20 21:57:10 -0700

    Databricks notebook cookiecutter

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Cookiecutter Databricks Notebook."""
+
+__author__ = """Teqniqly"""
+__email__ = 'farooq@teqniqly.com'
+__version__ = '0.1.0'
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -0,0 +1,17 @@
+"""
+The Notebook module.
+"""
+
+
+class Notebook:
+    """
+    This notebook does...
+    """
+    def run(self):
+        """
+        Your notebook code goes here.
+        """
+
+
+notebook = Notebook()
+notebook.run()
